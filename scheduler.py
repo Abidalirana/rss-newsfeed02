65ea87cbb2f409cbbb5f9cc862c8a72a7c46a137
abid ali <abidali431992@gmail.com>
2025-08-13 00:15:34 +0500

    change scheduler running time

--- scheduler.py
+++ scheduler.py
@@ -2,18 +2,15 @@ import asyncio
 import datetime
 from main import run_pipeline  # Adjust if needed
 
-
 async def job():
     print(f"⏰ Running job at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     await run_pipeline()
 
-
 async def scheduler():
     while True:
         await job()
-        await asyncio.sleep(10)  # Wait 10 seconds before next run
-
+        await asyncio.sleep(6 * 60 * 60)  # Sleep for 6 hours
 
 if __name__ == "__main__":
-    print("📆 Scheduler started... (will run every 10 seconds)")
+    print("📆 Scheduler started... (will run every 6 hours)")
     asyncio.run(scheduler())
